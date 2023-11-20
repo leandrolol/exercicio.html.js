c56e08fc326c308de2a8fa4a603594195c3caca2
leandrolol <146794496+leandrolol@users.noreply.github.com>
2023-11-20 19:03:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
 const form = document.getElementById('meuFormulario');
 
 function validaCampo (campoMaior){
-    const numeroComoArray = campoB.value > CampoC.value
-    return numeroComoArray.value 
+    const numeroComoArray = campoA.value > campoB.value;
+    return numeroComoArray.valueOf 
 }
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
+    const campoA = parseInt (document.getElementById('campoA').value);
     const campoB = parseInt (document.getElementById('campoB').value);
-    const CampoC = parseInt (document.getElementById('CampoC').value);
 
-    if (!validaCampo(campoB.value > CampoC.value)){
-        alert("Campo B não pode ser menor que Campo C ");
+    if (campoA > campoB){
+        const containerMensagemSucesso = document.querySelector('.sucesso');
+        containerMensagemSucesso.innerHTML = 'Parabéns!! O valor é válido';
+        containerMensagemSucesso.style.display = 'block';
+    form.reset()
     }else{
-        alert("Continuar");
+        form.campoA.style.border = '1px solid red';
+        form.campoB.style.border = '1px solid red';
+    
+        const containerMensagemErro = document.querySelector('.erro').style.display = 'block';
+        containerMensagemErro.innerHTML = ' Campo A não pode ser menor que Campo B';
+        containerMensagemErro.style.display = 'block';
     }
-});
+
+
+})
 
 console.log(form);
